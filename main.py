066640eaeaea10c08c967e13bc18797d311ae449
yo <tomchyshakvitalik@gmail.com>
2023-02-09 13:56:14 +0200

    Use webhook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import subprocess
+import os
 
 from aiogram import Bot, Dispatcher, executor
 from config import BOT_TOKEN, IP, URL
@@ -10,7 +11,7 @@ dp = Dispatcher(bot, loop=loop)
 
 
 async def main(dp):
-    # await bot.set_webhook(URL)
+    await bot.set_webhook(URL)
     old_response = ''
     # response = ping(IP, verbose=True)
     response = subprocess.getstatusoutput("ping -c 1 " + IP)
@@ -30,14 +31,14 @@ async def on_shutdown(dp):
 if __name__ == '__main__':
     from handlers import lights_off, lights_on
 
-    # executor.start_webhook(
-    #     dispatcher=dp,
-    #     webhook_path='',
-    #     on_startup=main,
-    #     on_shutdown=on_shutdown,
-    #     skip_updates=True,
-    #     host='0.0.0.0',
-    #     port=int(os.environ.get('PORT', 5000))
-    # )
-    executor.start_polling(dp, on_startup=main)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path='',
+        on_startup=main,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000))
+    )
+    # executor.start_polling(dp, on_startup=main)
 
