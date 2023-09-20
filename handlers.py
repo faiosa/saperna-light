2d87e8169d3d4e179d6a470989a2654cc34469ca
yo <tomchyshakvitalik@gmail.com>
2023-09-20 13:40:07 +0300

    Add feature

--- handlers.py
+++ handlers.py
@@ -1,6 +1,8 @@
 from main import bot
 from config import GROUP_ID, LIGHTS_ON_TEXT, LIGHTS_OFF_TEXT
 
+# Comment
+
 
 async def lights_on(dp):
     await bot.send_message(chat_id=GROUP_ID, text=LIGHTS_ON_TEXT)
